4f4c06bfb4f4606e57faa111e65772be3b9ec69f
John Robles <johncrm14@gmail.com>
2026-02-26 23:08:17 -0600

    feat: add terraform workflow and separate CI pipelines

---
+++ terraform/main.tf
@@ -0,0 +1,15 @@
+resource "aws_ecr_repository" "app_repo" {
+  name = var.project_name
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  tags = {
+    Environment = "dev"
+    Project     = var.project_name
+  }
+}
+resource "aws_s3_bucket" "test_bucket" {
+  bucket = "${var.project_name}-test-bucket-123456141099"
+}
